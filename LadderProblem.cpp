f797b180355c70c9b7f39f4db11a2bc1623729c0
kartikeya72001 <kartikeya72001@gmail.com>
2020-05-22 16:58:38 +0530

    LadderProblem

--- LadderProblem.cpp
+++ LadderProblem.cpp
@@ -22,11 +22,45 @@ int ladderTD(int n, int k, int dp[])
     return dp[n] = ways;
 }
 
+int ladderBU(int n, int k)
+{
+    int dp[100] = {0};
+    dp[0] = 1;
+    for(int i=1;i<=n;i++)
+    {
+        dp[i] = 0;
+        for(int j = 1;j<=k;j++)
+        {
+            if(i-j>=0)
+                dp[i] += dp[i-j];
+        }
+    }
+
+    return dp[n];
+}
+
+int ladderBUOpt(int n, int k)
+{
+    int dp[100] = {0};
+    dp[0] = dp[1] = 1;
+    for(int i=2;i<=n;i++)
+    {
+        if(i-k-1<0)
+            dp[i] = 2*dp[i-1];
+        else
+            dp[i] = 2*dp[i-1]-dp[i-k-1];
+    }
+
+    return dp[n];
+}
+
 int main(int argc, char const *argv[]) {
     int n = 4,k = 3;
     int dp[100] = {0};
     //cin>>n>>k;
-    cout<<ladderTD(n,k,dp)<<endl;
+    //cout<<ladderBU(n,k)<<endl;
+    cout<<ladderBUOpt(n,k)<<endl;
+    //cout<<ladderTD(n,k,dp)<<endl;
     cout<<clock()<<"ms";
     return 0;
 }
