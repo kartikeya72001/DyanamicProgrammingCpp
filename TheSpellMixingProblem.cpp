ac25aa903151f996617197fe6dbf9851fd115738
kartikeya72001 <kartikeya72001@gmail.com>
2020-05-24 15:46:28 +0530

    FriendsProblem

--- TheSpellMixingProblem.cpp
+++ TheSpellMixingProblem.cpp
@@ -35,7 +35,6 @@ int main(int argc, char const *argv[]) {
         for(int i=0;i<n;i++){
             cin>>arr[i];
         }
-        // cout<<"Arr: ";
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<n;j++)
