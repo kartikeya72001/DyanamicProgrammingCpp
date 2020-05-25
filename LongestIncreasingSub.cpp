7350463b30c6f36e93290b860b34f732b0a47121
kartikeya72001 <kartikeya72001@gmail.com>
2020-05-25 15:56:05 +0530

    LIS Updated

--- LongestIncreasingSub.cpp
+++ LongestIncreasingSub.cpp
@@ -27,13 +27,45 @@ int LIS(int n,int arr[])
 }
 
 int main(int argc, char const *argv[]) {
-    int n = 10, arr[] = {10,22,9,33,21,50,41,60,80,6};
+    int n = 5, arr[] = {1,3,5,4,7};
     // cin>>n;
     // for(int i=0;i<n;i++)
     // {
     //     cin>>arr[i];
     // }
-    cout<<"LIS is: "<<LIS(n,arr)+1<<endl;
+    cout<<"LIS is: "<<LIS(n,arr)<<endl;
     cout<<clock()<<"ms";
     return 0;
 }
+/*//NLOG(N) LIS
+#include <bits/stdc++.h>
+using namespace std;
+
+int LIS(int n, vector<int> nums)
+{
+    vector<int>lis;
+
+    for(int i=0 ; i<n ;i++){
+       auto it=lower_bound(begin(lis),end(lis),nums[i]);
+
+       if(it==lis.end())lis.push_back(nums[i]);
+
+       else *it=nums[i];
+    }
+    return lis.size();
+}
+
+int main(int argc, char const *argv[]) {
+    int n;
+	cin>>n;
+	vector<int> arr(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<LIS(n,arr)<<endl;
+    //cout<<clock()<<"ms";
+    return 0;
+}
+
+*/
